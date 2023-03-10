651f5805c422c762b3bf93b659e28171bcec14b3
Conor Schaefer <conor@ruin.dev>
2023-03-10 09:09:46 -0800

    feat: better error handling
    Snips out the 'unwraps' littered all over the place.
    Using map_err to anyhow because the scraper library's errors are not
    Send, which means anyhow can't wrap them by default.
    
    Includes some more docstrings as well.

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,31 @@
+//! A library and CLI utility for querying word etymologies
+//! from the inimitable [EtymOnline.com](https://etymonline.com).
+
 use anyhow::Result;
 use regex::Regex;
 use scraper::{Html, Selector};
-use std::option::Option;
 
+/// An etymology as retrieved from EtymOnline.com.
 pub struct Etymology {
+    /// The search term used for looking up an entry.
     pub word: String,
+    /// Container for the word, along with its part of speech, from the entry.
     pub label: String,
+    /// The entry as retrieved from EtymOnline.com. Currently this content
+    /// is formatted for terminal output, including bold and italics.
+    // TODO: add `etymology_html` field to make formatting optional.
     pub etymology: String,
 }
 
 impl Etymology {
-    pub fn new(word: &str) -> Result<Etymology> {
-        let results_html = query_etym_online(word);
-        let etymology_html = Etymology::extract_etymology_html(&results_html);
-        let etymology = Etymology::beautify(&etymology_html);
-        let label = Etymology::extract_word_name(&results_html).unwrap();
+    /// Performs a lookup via EtymOnline.com for the given word.
+    /// Fallible, as it can fail via network error, or simply
+    /// not find an entry. Error types are non-specific for simplicity's sake.
+    pub fn new(word: &str) -> Result<Self> {
+        let results_html = query_etym_online(word)?;
+        let etymology_html = Etymology::extract_etymology_html(&results_html)?;
+        let etymology = Etymology::beautify(&etymology_html)?;
+        let label = Etymology::extract_word_name(&results_html)?;
         Ok(Etymology {
             word: word.to_owned(),
             label,
@@ -24,72 +35,67 @@ impl Etymology {
 
     /// Substitute HTML formatting for italics with terminal escape codes.
     /// Does NOT intelligently determine whether terminal is interactive.
-    pub fn beautify(etym_html: &str) -> String {
-        let re_italics =
-            Regex::new(r#"<span class="\w+ notranslate">(?P<word>[^<]+)</span>"#).unwrap();
+    pub fn beautify(etym_html: &str) -> Result<String> {
+        let re_italics = Regex::new(r#"<span class="\w+ notranslate">(?P<word>[^<]+)</span>"#)?;
         // Use manual terminal escape codes for italics
         let e: String = re_italics
             .replace_all(etym_html, "\x1b[0;3m${word}\x1b[23m")
             .to_string();
         let html = Html::parse_fragment(&e);
         // Search for container "div" which was added in `extract_etymology_html` fn.
-        let sel = Selector::parse("div").unwrap();
-        html.select(&sel).next().unwrap().text().collect::<String>()
+        let sel = Selector::parse("div")
+            .map_err(|_| anyhow::anyhow!("Failed to find HTML div for beautification"))?;
+        Ok(html.select(&sel).next().unwrap().text().collect::<String>())
     }
 
     /// From raw HTML results of query, excise just the first definition found.
-    pub fn extract_etymology_html(raw_html: &str) -> String {
-        // TODO: Should return a Result, in case no hits were found.
+    pub fn extract_etymology_html(raw_html: &str) -> Result<String> {
         let d = Html::parse_document(raw_html);
-        let section_selector = Selector::parse("section").unwrap();
+        let section_selector = Selector::parse("section")
+            .map_err(|_| anyhow::anyhow!("Failed to parse HTML section for entry"))?;
         for x in d.select(&section_selector) {
             if let Some(y) = x.value().attr("class") {
                 if y.starts_with("word__def") {
                     // Pad with custom div, so we can easily retrieve the entirety again
                     // in `beautify`.
                     let etym_html = format!("<div>{}</div>", x.inner_html());
-                    return etym_html;
+                    return Ok(etym_html);
                 }
             }
         }
-        raw_html.to_string()
+        Ok(raw_html.to_string())
     }
 
     /// Extract the entry name, e.g. `Viking (n.)`
-    pub fn extract_word_name(raw_html: &str) -> Option<String> {
-        // TODO: Should return a Result, in case no hits were found.
+    pub fn extract_word_name(raw_html: &str) -> Result<String> {
         let d = Html::parse_document(raw_html);
-        let section_selector = Selector::parse("a").unwrap();
+        let section_selector = Selector::parse("a")
+            .map_err(|_| anyhow::anyhow!("Failed to parse 'a' element for word entry"))?;
         for x in d.select(&section_selector) {
             if let Some(y) = x.value().attr("class") {
                 if y.starts_with("word__name") {
                     // Pad with custom div, so we can easily retrieve the entirety again
                     // in `beautify`.
                     let word_name = x.text().collect::<String>();
-                    return Some(word_name);
+                    return Ok(word_name);
                 }
             }
         }
-        None
+        anyhow::bail!("Failed to find word name within HTML")
     }
 }
 
 /// Perform HTTP GET to query EtymOnline.com.
 /// Requires a search term. Currently NOT URL-encoded.
 /// Returns raw HTML results.
-fn query_etym_online(word: &str) -> String {
+fn query_etym_online(word: &str) -> Result<String> {
     // TODO: we should urlescape the word, in case it has spaces
     let url = format!("https://www.etymonline.com/search?q={}", word);
     // Fetch HTML
-    match ureq::get(&url).call() {
-        Ok(response) => response.into_string().unwrap(),
-        Err(ureq::Error::Status(code, _response)) => {
-            panic!("Encountered HTTP error {} when looking up {}", code, word);
-        }
-        Err(_) => {
-            panic!("Failed to look up {}", word);
-        }
-    }
+    ureq::get(&url)
+        .call()?
+        .into_string()
+        .map_err(|_| anyhow::anyhow!(format!("Failed to query EtymOnline; network error?")))
 }
 
 #[cfg(test)]
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use clap::{arg, command};
 
 // static APP_USER_AGENT: &str = concat!(env!("CARGO_PKG_NAME"), "/", env!("CARGO_PKG_VERSION"));
 
-fn main() {
+fn main() -> anyhow::Result<()> {
+    // TODO 'word' should be a manadatory argument
     let matches = command!()
         .arg(arg!([word] "Word for which etymology is desired"))
         .arg(arg!(
@@ -11,11 +12,22 @@ fn main() {
         .get_matches();
 
     if let Some(word) = matches.value_of("word") {
-        let e = etym::Etymology::new(word).unwrap();
+        let e = match etym::Etymology::new(word) {
+            Ok(r) => r,
+            Err(_) => {
+                return Err(anyhow::anyhow!(format!(
+                    "Failed to find an etymology for '{}'",
+                    word
+                )))
+            }
+        };
         // Manually bold the search term
         println!("\x1b[1m{}\x1b[22m", e.label);
         let w = textwrap::termwidth();
         let s = textwrap::fill(&e.etymology, w);
         println!("{}", s);
+    } else {
+        return Err(anyhow::anyhow!("No word to search for was supplied"));
     }
+    Ok(())
 }
